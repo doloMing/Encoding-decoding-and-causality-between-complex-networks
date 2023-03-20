60a44c8dd1223b8687b98c3648cc718cca5420dc
Yang Tyan (Yang Tian) <35678646+doloMing@users.noreply.github.com>
2023-03-20 14:36:16 +0800

    Delete PinvFunction.m

--- PinvFunction.m
+++
@@ -1,6 +0,0 @@
-function PinvL=PinvFunction(L)
-% Input: 
-% L£º the Laplacian of network
-% Output: 
-% PinvL: the Moore¨CPenrose pseudoinverse of L
-PinvL=inv(L+ones(size(L,1),size(L,1)).*1/size(L,1))-ones(size(L,1),size(L,1)).*1/size(L,1);
