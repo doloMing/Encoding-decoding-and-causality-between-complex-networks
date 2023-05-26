fda149ebfd0a625a4be8b6a721049968303221a2
Yang Tyan (Yang Tian) <35678646+doloMing@users.noreply.github.com>
2023-05-26 19:18:44 +0800

    Update fisher_info.py

--- PythonCode/toolkit/fisher_info.py
+++ PythonCode/toolkit/fisher_info.py
@@ -21,30 +21,22 @@ def fisher_information(sigma_ensemble, theta_matrix):
 
     assert theta_matrix.shape[0] == sigma_ensemble.shape[0]
     x, k = theta_matrix.shape
-    n = sigma_ensemble.shape[1]
     
     derivatives = []
+    sigma_ensemble_diff = np.diff(sigma_ensemble, axis=0)
     for t in range(k):
-        derivative_matrix = np.zeros((x-1, n, n))
-        for i in range(n):
-            for j in range(n):
-                needed_s = sigma_ensemble[:, i, j]
-                # print(needed_s)
-                derivative_vec = np.diff(needed_s, axis=0) / (np.diff(theta_matrix[:, t], axis=0) + np.finfo(float).eps)
-                # print(derivative_vec)
-                derivative_vec[np.diff(theta_matrix[:, t], axis=0) == 0] = 0
-                derivative_matrix[:, i, j] = derivative_vec
+        derivative_matrix = sigma_ensemble_diff / (np.diff(theta_matrix[:, t], axis=0) + np.finfo(float).eps).reshape(-1,1,1)
+        derivative_matrix[np.diff(theta_matrix[:, t], axis=0) == 0] = 0
         derivatives.append(derivative_matrix)
     derivatives = np.array(derivatives)
 
     fisher_info_matrix = np.zeros((x-1, k, k))
     for i in range(x-1):
+        sigma_ensemble_i_inv = np.linalg.inv(sigma_ensemble[i])
         for j in range(k):
-            derivative_matrix_1 = derivatives[j]
+            derivative_matrix_1 = derivatives[j, i]
             for l in range(k):
-                derivative_matrix_2 = derivatives[l]
-                sigma_ensemble_i_inv = np.linalg.inv(sigma_ensemble[i])
+                derivative_matrix_2 = derivatives[l, i]
                 fisher_info_matrix[i, j, l] = 0.5 * np.trace(np.matmul(sigma_ensemble_i_inv, 
-                        np.matmul(derivative_matrix_1[i], np.dot(sigma_ensemble_i_inv, derivative_matrix_2[i]))))
-                
+                        np.matmul(derivative_matrix_1, np.dot(sigma_ensemble_i_inv, derivative_matrix_2))))
     return fisher_info_matrix
