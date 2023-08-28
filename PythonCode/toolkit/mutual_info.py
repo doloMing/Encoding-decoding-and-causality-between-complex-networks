2be5d43816d3f8ba375e55835052cd96315bc88b
Yang Tyan (Yang Tian) <35678646+doloMing@users.noreply.github.com>
2023-08-28 16:04:07 +0800

    Update mutual_info.py

--- PythonCode/toolkit/mutual_info.py
+++ PythonCode/toolkit/mutual_info.py
@@ -23,7 +23,17 @@ def mutual_infomation(sigma_a, sigma_b, sample_num, k):
     h_b = (1.0 + np.log(2 * np.pi)) * sigma_b.shape[0] / 2.0 + np.sum(np.log(np.linalg.eigvals(sigma_b))) / 2.0
     h_ab = entropy_estimation(joint_samples, k)
 
-    mi = np.max(h_a + h_b - h_ab, 0)
-    if mi < 0:
-        mi = 0.0
+    h_a = np.real((1.0 + np.log(2 * np.pi)) * sigma_a.shape[0] / 2.0 + np.sum(np.log(np.linalg.eigvals(sigma_a))) / 2.0)
+    h_b = np.real((1.0 + np.log(2 * np.pi)) * sigma_b.shape[0] / 2.0 + np.sum(np.log(np.linalg.eigvals(sigma_b))) / 2.0)
+
+    if h_a<0.0:
+        h_a=0.0
+
+    if h_b<0.0:
+        h_b=0.0
+
+    h_ab = entropy_estimation(joint_samples, k)
+
+    mi = np.min([np.max([h_a + h_b - h_ab, 0]),h_a,h_b])
+
     return h_a, h_b, h_ab, mi
